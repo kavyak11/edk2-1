72a76253dc5df961f649dd363cb38d84ce9cbd7e
Zhiguang Liu <zhiguang.liu@intel.com>
2022-03-22 14:21:13 +0800

    OvmfPkg: Ovmf edk2 pol will create cbor buffer as payload interface
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

--- OvmfPkg/QemuUniversalPayload/UniversalPayloadInitializationPei/UniversalPayloadInitializationPei.c
+++ OvmfPkg/QemuUniversalPayload/UniversalPayloadInitializationPei/UniversalPayloadInitializationPei.c
@@ -19,9 +19,9 @@
 #include <Library/QemuFwCfgLib.h>
 #include <OvmfPlatforms.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/SetUplDataLib.h>
 
-STATIC UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE mNonExistAperture = { MAX_UINT64, 0 };
-
+STATIC UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  mNonExistAperture = { MAX_UINT64, 0 };
 
 /**
   Utility function to initialize a PCI_ROOT_BRIDGE structure.
@@ -70,27 +70,24 @@ STATIC UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE mNonExistAperture = { MAX_UINT
 
   @retval EFI_OUT_OF_RESOURCES       Memory allocation failed.
 **/
-
 EFI_STATUS
 EFIAPI
 PeiPciHostBridgeUtilityInitRootBridge (
-  IN  UINT64                   Supports,
-  IN  UINT64                   Attributes,
-  IN  UINT64                   AllocAttributes,
-  IN  BOOLEAN                  DmaAbove4G,
-  IN  BOOLEAN                  NoExtendedConfigSpace,
-  IN  UINT8                    RootBusNumber,
-  IN  UINT8                    MaxSubBusNumber,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *Io,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *Mem,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *MemAbove4G,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *PMem,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *PMemAbove4G,
-  OUT UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE      *RootBus
+  IN  UINT64                                      Supports,
+  IN  UINT64                                      Attributes,
+  IN  UINT64                                      AllocAttributes,
+  IN  BOOLEAN                                     DmaAbove4G,
+  IN  BOOLEAN                                     NoExtendedConfigSpace,
+  IN  UINT8                                       RootBusNumber,
+  IN  UINT8                                       MaxSubBusNumber,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *Io,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *Mem,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *MemAbove4G,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *PMem,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *PMemAbove4G,
+  OUT UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE           *RootBus
   )
 {
-
-
   //
   // Be safe if other fields are added to PCI_ROOT_BRIDGE later.
   //
@@ -104,8 +101,8 @@ PeiPciHostBridgeUtilityInitRootBridge (
   RootBus->DmaAbove4G = DmaAbove4G;
 
   RootBus->AllocationAttributes = AllocAttributes;
-  RootBus->Bus.Base  = RootBusNumber;
-  RootBus->Bus.Limit = MaxSubBusNumber;
+  RootBus->Bus.Base             = RootBusNumber;
+  RootBus->Bus.Limit            = MaxSubBusNumber;
   CopyMem (&RootBus->Io, Io, sizeof (*Io));
   CopyMem (&RootBus->Mem, Mem, sizeof (*Mem));
   CopyMem (&RootBus->MemAbove4G, MemAbove4G, sizeof (*MemAbove4G));
@@ -114,14 +111,16 @@ PeiPciHostBridgeUtilityInitRootBridge (
 
   RootBus->NoExtendedConfigSpace = NoExtendedConfigSpace;
 
-
   RootBus->UID = RootBusNumber;
-  RootBus->HID = EISA_PNP_ID(0x0A03);
+  RootBus->HID = EISA_PNP_ID (0x0A03);
 
-
-  DEBUG ((DEBUG_INFO,
+  DEBUG ((
+    DEBUG_INFO,
     "%a: populated root bus %d, with room for %d subordinate bus(es)\n",
-    __FUNCTION__, RootBusNumber, MaxSubBusNumber - RootBusNumber));
+    __FUNCTION__,
+    RootBusNumber,
+    MaxSubBusNumber - RootBusNumber
+    ));
   return EFI_SUCCESS;
 }
 
@@ -154,38 +153,43 @@ PeiPciHostBridgeUtilityInitRootBridge (
 
   @return                            All the root bridge instances in an array.
 **/
-
 UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE *
 EFIAPI
 PeiPciHostBridgeUtilityGetRootBridges (
-  OUT UINTN                    *Count,
-  IN  UINT64                   Attributes,
-  IN  UINT64                   AllocationAttributes,
-  IN  BOOLEAN                  DmaAbove4G,
-  IN  BOOLEAN                  NoExtendedConfigSpace,
-  IN  UINTN                    BusMin,
-  IN  UINTN                    BusMax,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *Io,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *Mem,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *MemAbove4G,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *PMem,
-  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE *PMemAbove4G
+  OUT UINTN                                       *Count,
+  IN  UINT64                                      Attributes,
+  IN  UINT64                                      AllocationAttributes,
+  IN  BOOLEAN                                     DmaAbove4G,
+  IN  BOOLEAN                                     NoExtendedConfigSpace,
+  IN  UINTN                                       BusMin,
+  IN  UINTN                                       BusMax,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *Io,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *Mem,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *MemAbove4G,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *PMem,
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  *PMemAbove4G
   )
 {
-  EFI_STATUS           Status;
-  FIRMWARE_CONFIG_ITEM FwCfgItem;
-  UINTN                FwCfgSize;
-  UINT64               ExtraRootBridges;
+  EFI_STATUS                         Status;
+  FIRMWARE_CONFIG_ITEM               FwCfgItem;
+  UINTN                              FwCfgSize;
+  UINT64                             ExtraRootBridges;
   UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE  *Bridges;
-  UINTN                Initialized;
-  UINTN                LastRootBridgeNumber;
-  UINTN                RootBridgeNumber;
+  UINTN                              Initialized;
+  UINTN                              LastRootBridgeNumber;
+  UINTN                              RootBridgeNumber;
 
   *Count = 0;
 
-  if (BusMin > BusMax || BusMax > PCI_MAX_BUS) {
-    DEBUG ((DEBUG_ERROR, "%a: invalid bus range with BusMin %Lu and BusMax "
-      "%Lu\n", __FUNCTION__, (UINT64)BusMin, (UINT64)BusMax));
+  if ((BusMin > BusMax) || (BusMax > PCI_MAX_BUS)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: invalid bus range with BusMin %Lu and BusMax "
+      "%Lu\n",
+      __FUNCTION__,
+      (UINT64)BusMin,
+      (UINT64)BusMax
+      ));
     return NULL;
   }
 
@@ -194,7 +198,7 @@ PeiPciHostBridgeUtilityGetRootBridges (
   // search below. If there is no hint, the feature is missing.
   //
   Status = QemuFwCfgFindFile ("etc/extra-pci-roots", &FwCfgItem, &FwCfgSize);
-  if (EFI_ERROR (Status) || FwCfgSize != sizeof ExtraRootBridges) {
+  if (EFI_ERROR (Status) || (FwCfgSize != sizeof ExtraRootBridges)) {
     ExtraRootBridges = 0;
   } else {
     QemuFwCfgSelectItem (FwCfgItem);
@@ -208,12 +212,22 @@ PeiPciHostBridgeUtilityGetRootBridges (
     // invalid behavior.
     //
     if (ExtraRootBridges > BusMax - BusMin) {
-      DEBUG ((DEBUG_ERROR, "%a: invalid count of extra root buses (%Lu) "
-        "reported by QEMU\n", __FUNCTION__, ExtraRootBridges));
+      DEBUG ((
+        DEBUG_ERROR,
+        "%a: invalid count of extra root buses (%Lu) "
+        "reported by QEMU\n",
+        __FUNCTION__,
+        ExtraRootBridges
+        ));
       return NULL;
     }
-    DEBUG ((DEBUG_INFO, "%a: %Lu extra root buses reported by QEMU\n",
-      __FUNCTION__, ExtraRootBridges));
+
+    DEBUG ((
+      DEBUG_INFO,
+      "%a: %Lu extra root buses reported by QEMU\n",
+      __FUNCTION__,
+      ExtraRootBridges
+      ));
   }
 
   //
@@ -224,6 +238,7 @@ PeiPciHostBridgeUtilityGetRootBridges (
     DEBUG ((DEBUG_ERROR, "%a: %r\n", __FUNCTION__, EFI_OUT_OF_RESOURCES));
     return NULL;
   }
+
   Initialized = 0;
 
   //
@@ -238,15 +253,24 @@ PeiPciHostBridgeUtilityGetRootBridges (
   //
   for (RootBridgeNumber = BusMin + 1;
        RootBridgeNumber <= BusMax && Initialized < ExtraRootBridges;
-       ++RootBridgeNumber) {
-    UINTN Device;
+       ++RootBridgeNumber)
+  {
+    UINTN  Device;
 
     for (Device = 0; Device <= PCI_MAX_DEVICE; ++Device) {
-      if (PciRead16 (PCI_LIB_ADDRESS (RootBridgeNumber, Device, 0,
-                       PCI_VENDOR_ID_OFFSET)) != MAX_UINT16) {
+      if (PciRead16 (
+            PCI_LIB_ADDRESS (
+              RootBridgeNumber,
+              Device,
+              0,
+              PCI_VENDOR_ID_OFFSET
+              )
+            ) != MAX_UINT16)
+      {
         break;
       }
     }
+
     if (Device <= PCI_MAX_DEVICE) {
       //
       // Found the next root bus. We can now install the *previous* one,
@@ -254,23 +278,24 @@ PeiPciHostBridgeUtilityGetRootBridges (
       // subordinate buses that might exist behind PCI bridges hanging off it.
       //
       Status = PeiPciHostBridgeUtilityInitRootBridge (
-        Attributes,
-        Attributes,
-        AllocationAttributes,
-        DmaAbove4G,
-        NoExtendedConfigSpace,
-        (UINT8) LastRootBridgeNumber,
-        (UINT8) (RootBridgeNumber - 1),
-        Io,
-        Mem,
-        MemAbove4G,
-        PMem,
-        PMemAbove4G,
-        &Bridges[Initialized]
-        );
+                 Attributes,
+                 Attributes,
+                 AllocationAttributes,
+                 DmaAbove4G,
+                 NoExtendedConfigSpace,
+                 (UINT8)LastRootBridgeNumber,
+                 (UINT8)(RootBridgeNumber - 1),
+                 Io,
+                 Mem,
+                 MemAbove4G,
+                 PMem,
+                 PMemAbove4G,
+                 &Bridges[Initialized]
+                 );
       if (EFI_ERROR (Status)) {
         goto FreeBridges;
       }
+
       ++Initialized;
       LastRootBridgeNumber = RootBridgeNumber;
     }
@@ -281,38 +306,38 @@ PeiPciHostBridgeUtilityGetRootBridges (
   // we've found no extra root buses).
   //
   Status = PeiPciHostBridgeUtilityInitRootBridge (
-    Attributes,
-    Attributes,
-    AllocationAttributes,
-    DmaAbove4G,
-    NoExtendedConfigSpace,
-    (UINT8) LastRootBridgeNumber,
-    (UINT8) BusMax,
-    Io,
-    Mem,
-    MemAbove4G,
-    PMem,
-    PMemAbove4G,
-    &Bridges[Initialized]
-    );
+             Attributes,
+             Attributes,
+             AllocationAttributes,
+             DmaAbove4G,
+             NoExtendedConfigSpace,
+             (UINT8)LastRootBridgeNumber,
+             (UINT8)BusMax,
+             Io,
+             Mem,
+             MemAbove4G,
+             PMem,
+             PMemAbove4G,
+             &Bridges[Initialized]
+             );
   if (EFI_ERROR (Status)) {
     goto FreeBridges;
   }
+
   ++Initialized;
 
   *Count = Initialized;
   return Bridges;
 
 FreeBridges:
-  //while (Initialized > 0) {
+  // while (Initialized > 0) {
   //  --Initialized;
   //  PciHostBridgeUtilityUninitRootBridge (&Bridges[Initialized]);
-  //}
+  // }
 
   FreePool (Bridges);
   return NULL;
 }
-
 
 /**
   Return all the root bridge instances in an array.
@@ -323,62 +348,60 @@ FreeBridges:
           The array should be passed into PciHostBridgeFreeRootBridges()
           when it's not used.
 **/
-
 UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE *
 EFIAPI
 PeiPciHostBridgeGetRootBridges (
-  UINTN *Count
+  UINTN  *Count
   )
 {
-  UINT64               Attributes;
-  UINT64               AllocationAttributes;
-  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE Io;
-  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE Mem;
-  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE MemAbove4G;
+  UINT64                                      Attributes;
+  UINT64                                      AllocationAttributes;
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  Io;
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  Mem;
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE_APERTURE  MemAbove4G;
 
   ZeroMem (&Io, sizeof (Io));
   ZeroMem (&Mem, sizeof (Mem));
   ZeroMem (&MemAbove4G, sizeof (MemAbove4G));
 
   Attributes = EFI_PCI_ATTRIBUTE_IDE_PRIMARY_IO |
-    EFI_PCI_ATTRIBUTE_IDE_SECONDARY_IO |
-    EFI_PCI_ATTRIBUTE_ISA_IO_16 |
-    EFI_PCI_ATTRIBUTE_ISA_MOTHERBOARD_IO |
-    EFI_PCI_ATTRIBUTE_VGA_MEMORY |
-    EFI_PCI_ATTRIBUTE_VGA_IO_16 |
-    EFI_PCI_ATTRIBUTE_VGA_PALETTE_IO_16;
+               EFI_PCI_ATTRIBUTE_IDE_SECONDARY_IO |
+               EFI_PCI_ATTRIBUTE_ISA_IO_16 |
+               EFI_PCI_ATTRIBUTE_ISA_MOTHERBOARD_IO |
+               EFI_PCI_ATTRIBUTE_VGA_MEMORY |
+               EFI_PCI_ATTRIBUTE_VGA_IO_16 |
+               EFI_PCI_ATTRIBUTE_VGA_PALETTE_IO_16;
 
   AllocationAttributes = 1;
   if (PcdGet64 (PcdPciMmio64Size) > 0) {
     AllocationAttributes |= 2;
-    MemAbove4G.Base = PcdGet64 (PcdPciMmio64Base);
-    MemAbove4G.Limit = PcdGet64 (PcdPciMmio64Base) +
-                       PcdGet64 (PcdPciMmio64Size) - 1;
+    MemAbove4G.Base       = PcdGet64 (PcdPciMmio64Base);
+    MemAbove4G.Limit      = PcdGet64 (PcdPciMmio64Base) +
+                            PcdGet64 (PcdPciMmio64Size) - 1;
   } else {
     CopyMem (&MemAbove4G, &mNonExistAperture, sizeof (mNonExistAperture));
   }
 
-  Io.Base = PcdGet64 (PcdPciIoBase);
-  Io.Limit = PcdGet64 (PcdPciIoBase) + (PcdGet64 (PcdPciIoSize) - 1);
-  Mem.Base = PcdGet64 (PcdPciMmio32Base);
+  Io.Base   = PcdGet64 (PcdPciIoBase);
+  Io.Limit  = PcdGet64 (PcdPciIoBase) + (PcdGet64 (PcdPciIoSize) - 1);
+  Mem.Base  = PcdGet64 (PcdPciMmio32Base);
   Mem.Limit = PcdGet64 (PcdPciMmio32Base) + (PcdGet64 (PcdPciMmio32Size) - 1);
 
   return PeiPciHostBridgeUtilityGetRootBridges (
-    Count,
-    Attributes,
-    AllocationAttributes,
-    FALSE,
-    PcdGet16 (PcdOvmfHostBridgePciDevId) != INTEL_Q35_MCH_DEVICE_ID,
-    0,
-    PCI_MAX_BUS,
-    &Io,
-    &Mem,
-    &MemAbove4G,
-    &mNonExistAperture,
-    &mNonExistAperture
-    );
+           Count,
+           Attributes,
+           AllocationAttributes,
+           FALSE,
+           PcdGet16 (PcdOvmfHostBridgePciDevId) != INTEL_Q35_MCH_DEVICE_ID,
+           0,
+           PCI_MAX_BUS,
+           &Io,
+           &Mem,
+           &MemAbove4G,
+           &mNonExistAperture,
+           &mNonExistAperture
+           );
 }
-
 
 /**
   Publish the FV that includes the UPL and Publish the HOBs required by UPL.
@@ -396,65 +419,25 @@ UniversalPayloadInitialization (
   IN CONST EFI_PEI_SERVICES     **PeiServices
   )
 {
-  UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO  *Serial;
-  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES  *PciRootBridgeInfo;
-  UINT16                              HostBridgeDevId;
-  UINTN                               Pmba;
-  EFI_FIRMWARE_VOLUME_HEADER          *UplFv;
-  ACPI_BOARD_INFO                     *AcpiBoardInfo;
+  EFI_FIRMWARE_VOLUME_HEADER         *UplFv;
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE  *RootBridge;
+  UINTN                              RootBridgeCount;
 
-  Pmba = 0;
   DEBUG ((DEBUG_INFO, "=====================Report UPL FV=======================================\n"));
-  UplFv = (EFI_FIRMWARE_VOLUME_HEADER *) PcdGet32 (PcdOvmfPldFvBase);
+  UplFv = (EFI_FIRMWARE_VOLUME_HEADER *)PcdGet32 (PcdOvmfPldFvBase);
   ASSERT (UplFv->FvLength == PcdGet32 (PcdOvmfPldFvSize));
-  PeiServicesInstallFvInfoPpi (&UplFv->FileSystemGuid, UplFv, (UINT32) UplFv->FvLength, NULL, NULL);
+  PeiServicesInstallFvInfoPpi (&UplFv->FileSystemGuid, UplFv, (UINT32)UplFv->FvLength, NULL, NULL);
 
   DEBUG ((DEBUG_INFO, "=====================Build UPL HOBs=======================================\n"));
-  //
-  // Query Host Bridge DID to determine platform type
-  //
-  HostBridgeDevId = PcdGet16 (PcdOvmfHostBridgePciDevId);
-  switch (HostBridgeDevId) {
-    case INTEL_82441_DEVICE_ID:
-      Pmba = POWER_MGMT_REGISTER_PIIX4 (PIIX4_PMBA);
-      break;
-    case INTEL_Q35_MCH_DEVICE_ID:
-      Pmba = POWER_MGMT_REGISTER_Q35 (ICH9_PMBASE);
-      break;
-    default:
-      DEBUG ((DEBUG_ERROR, "%a: Unknown Host Bridge Device ID: 0x%04x\n",
-        __FUNCTION__, HostBridgeDevId));
-      ASSERT (FALSE);
-  }
 
+  SetUplUint64 (UPL_KEY_SERIAL_PORT_BAUDRATE, (UINT64)PcdGet32 (PcdSerialBaudRate));
+  SetUplBoolean (UPL_KEY_SERIAL_PORT_USE_MMIO, PcdGetBool (PcdSerialUseMmio));
+  SetUplUint64 (UPL_KEY_SERIAL_PORT_REGISTER_BASE, (UINT64)PcdGet64 (PcdSerialRegisterBase));
+  SetUplUint64 (UPL_KEY_SERIAL_PORT_REGISTER_STRIDE, (UINT64)PcdGet32 (PcdSerialRegisterStride));
 
-  Serial = BuildGuidHob (&gUniversalPayloadSerialPortInfoGuid, sizeof (UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO));
-  Serial->BaudRate = PcdGet32 (PcdSerialBaudRate);
-  Serial->RegisterBase = PcdGet64 (PcdSerialRegisterBase);
-  Serial->RegisterStride = (UINT8) PcdGet32 (PcdSerialRegisterStride);
-  Serial->Header.Revision = UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO_REVISION;
-  Serial->Header.Length = sizeof (UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO);
-  Serial->UseMmio = PcdGetBool (PcdSerialUseMmio);
-
-  AcpiBoardInfo = BuildGuidHob (&gUefiAcpiBoardInfoGuid, sizeof (ACPI_BOARD_INFO));
-  AcpiBoardInfo->PcieBaseAddress = PcdGet64 (PcdPciExpressBaseAddress);
-  AcpiBoardInfo->PcieBaseSize = SIZE_256MB;
-
-  AcpiBoardInfo->PmTimerRegBase = (PciRead32 (Pmba) & ~PMBA_RTE) + ACPI_TIMER_OFFSET;
-
-  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE * RootBridge;
-  UINTN         RootBridgeCount;
-  RootBridge = PeiPciHostBridgeGetRootBridges(&RootBridgeCount);
-
-  PciRootBridgeInfo = BuildGuidHob (&gUniversalPayloadPciRootBridgeInfoGuid, sizeof (PciRootBridgeInfo) + sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE));
-  CopyMem(PciRootBridgeInfo->RootBridge, RootBridge, sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE));
-  PciRootBridgeInfo->Count = (UINT8)RootBridgeCount;
-  PciRootBridgeInfo->Header.Length = sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE);
-  PciRootBridgeInfo->Header.Revision =  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES_REVISION;
-  DEBUG ((DEBUG_ERROR, "%a: PciRootBridgeInfo->Count: 0x%04x\n",  __FUNCTION__, RootBridgeCount));
-  PciRootBridgeInfo->ResourceAssigned = FALSE;
-  DEBUG ((DEBUG_ERROR, "%a: PciRootBridgeInfo->RootBridge[0].ResourceAssigned: 0x%04x\n",  __FUNCTION__, PciRootBridgeInfo->ResourceAssigned));
-  DEBUG ((DEBUG_ERROR, "%a: PciRootBridgeInfo->RootBridge[0].ResourceAssigned: 0x%x\n",  __FUNCTION__, (UINTN)PciRootBridgeInfo->RootBridge[0].Bus.Limit));
+  RootBridge = PeiPciHostBridgeGetRootBridges (&RootBridgeCount);
+  SetUplPciRootBridges (RootBridge, RootBridgeCount);
+  SetUplBoolean (UPL_KEY_ROOT_BRIDGE_RESOURCE_ASSIGNED, FALSE);
 
   return EFI_SUCCESS;
 }
